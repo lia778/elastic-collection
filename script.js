3f34b539deb9644ab279aa154b5065c9953c16d7
lia778 <lia778@newschool.edu>
2023-03-24 10:27:15 -0400

    fuck airtable

--- script.js
+++ script.js
@@ -1,287 +1,72 @@
+// activate airtable object
 var Airtable = require("airtable");
-var base = new Airtable({
-  apiKey: "keyCteapyZ1OyWKMD",
-}).base("appUX3a2EFTq8Lc9h");
-// create empty array to use later for filters
-let locations = [];
 
-// find the parent container element to which we will append each record
-let container = document.querySelector(".content-container");
+var base = new Airtable(
+  {apiKey:"keyCteapyZ1OyWKMD"}
+).base("appgp5WUx7Qk5bSfm");
 
-base("Table 1")
-  .select({
-    // Selecting the first 50 records in Grid view:
+base("Table 1").select({
     maxRecords: 50,
-    view: "Grid view",
-  })
-  .eachPage(
-    function page(records, fetchNextPage) {
-      console.log("records",records);
-      // This function (`page`) will get called for each page of records.
-      records.forEach(function (record, index) {
-        // loop through each year in this record
-        record.fields.location.forEach((location) => {
-          // if the year is not already in the years array
-          // add it to the years array
-          if (!locations.includes(location)) locations.push(location);
-        });
-
-        // create div element for each record
+    // view: "Grid view",
+}).eachPage(
+  function page(records, fetchNextPage){
+    console.log("records:", records); 
+    records.forEach(
+      function (record) {
+        // pull my airtable data 
+        // each record will have its own div
         let airtableItem = document.createElement("div");
-        // add a class to the record element
+        // add some data specific meta to my new divs for filtering
         airtableItem.classList.add("airtable-item");
-        // set the data-year attribute equal to the value of the record year
-        // this will be used to sort items
-        airtableItem.setAttribute("data-year", record.fields.year);
-        // set the data-location attribute equal to the value of the record location
-        // this will be used to filter items
-        airtableItem.setAttribute("data-location", record.fields.location);
-
-        /*
-        drawer title button
-        */
-
-        // create a button element hold the title, artist, and year
-        let drawerButton = document.createElement("button");
-        // add a class to the button element
-        drawerButton.classList.add("drawer-button");
-
-        // create an h2 element for the title
-        let drawerButtonTitle = document.createElement("h2");
-        // add a class to the button element
-        drawerButtonTitle.classList.add("drawer-button--title");
-        // render the value of the record's title
-        drawerButtonTitle.innerHTML = record.fields.title;
-        // append the button element to the airtableItem div element created above
-        drawerButton.append(drawerButtonTitle);
-
-        // create a button element hold the title, artist, and year
-        let drawerButtonInfo = document.createElement("div");
-        // add a class to the button element
-        drawerButtonInfo.classList.add("drawer-button--info");
-        // render the value of the record's title
-        drawerButtonInfo.innerHTML = `${record.fields.artist}, ${record.fields.year[0]}`;
-        // append the button element to the airtableItem div element created above
-        drawerButton.append(drawerButtonInfo);
-
-        // append the button element to the airtableItem div element created above
-        airtableItem.append(drawerButton);
-
-        /*
-        drawer content
-        */
-
-        // create a div element for the record's description, link, and image
-        let drawerContent = document.createElement("div");
-        // add a class to the img element
-        drawerContent.classList.add("drawer-content");
-        // if this is the first item in the for loop
-        // add class to open it initially
-        if (index == 0) {
-          airtableItem.classList.add("is-open");
-        }
-
-        // create a div element for the record's description
-        let drawerContentDescription = document.createElement("div");
-        // add a class to the description element
-        drawerContentDescription.classList.add("drawer-content--description");
-        // render the record's desription in the html
-        drawerContentDescription.innerHTML = record.fields.description;
-        // append the div element to the airtableItem div element created above
-        drawerContent.append(drawerContentDescription);
-
-        // create a div element for the record's description
-        let drawerContentImageTable = document.createElement("div");
-        // add a class to the description element
-        drawerContentImageTable.classList.add("drawer-content--imageTable");
-        // append the div element to the airtableItem div element created above
-        drawerContent.append(drawerContentImageTable);
-
-        // create an img element for the record's image
-        let drawerContentImage = document.createElement("img");
-        // add a class to the image element
-        drawerContentImage.classList.add("drawer-content--image");
-        // set the source of the record's image
-        drawerContentImage.src = record.fields.cover_image[0].url;
-        // append the img element to the image container element created above
-        drawerContentImageTable.append(drawerContentImage);
-
-        // append the drawer content element to the airtableItem div element created above
-        airtableItem.append(drawerContent);
-
-        // once all elements are created for this record,
-        // append it to the parent container
-        container.append(airtableItem);
-
-        // create drawer toggle function
-        drawerButton.addEventListener("click", () => {
-          airtableItem.classList.toggle("is-open");
-        });
-      });
-
-      // To fetch the next page of records, call `fetchNextPage`.
-      // If there are more records, `page` will get called again.
-      // If there are no more records, `done` will get called.
-      fetchNextPage();
-    },
-    function done(err) {
-      if (err) {
-        console.error(err);
-        return;
-      } else {
-        // get all of the rendered records
-        let allItems = Array.from(document.querySelectorAll(".airtable-item"));
-
-        /* 
-        SORT
-        */
-
-        // create the sort function
-        function sortItems(e) {
-          // close all drawers after sorting
-          document
-            .querySelectorAll(".airtable-item.is-open")
-            .forEach((item) => {
-              item.classList.remove("is-open");
-            });
-
-          // only sort through filtered items
-          let filteredItems = Array.from(
-            document.querySelectorAll(".airtable-item")
-          );
-          let sortedItems;
-
-          // NOTE: below is an example of sorting alphabetical values like titles
-
-          // if toggle data-type == ascend (a-z)
-          // if (e?.target.dataset.type == "ascend") {
-          //   e.target.setAttribute("data-type", "descend");
-
-          //   sortedItems = filteredItems.sort(function (a, b) {
-          //     return a.dataset.title.localeCompare(b.dataset.title);
-          //   });
-          // } else {
-          //   e.target.setAttribute("data-type", "ascend");
-
-          //   sortedItems = filteredItems.sort(function (a, b) {
-          //     return b.dataset.title.localeCompare(a.dataset.title);
-          //   });
-          // }
-
-          // NOTE: below is an example of toggling numeric values like dates
-
-          // check current sort type and compare elements
-          if (e?.target.dataset.type == "descend") {
-            e?.target.setAttribute("data-type", "ascend");
-            e.target.innerHTML = "NEWEST";
-
-            sortedItems = filteredItems.sort(function (a, b) {
-              return (
-                parseInt(a.dataset.year.toString()) -
-                parseInt(b.dataset.year.toString())
-              );
-            });
-          } else {
-            e?.target.setAttribute("data-type", "descend");
-            e.target.innerHTML = "OLDEST";
-
-            sortedItems = filteredItems.sort(function (a, b) {
-              return (
-                parseInt(b.dataset.year.toString()) -
-                parseInt(a.dataset.year.toString())
-              );
-            });
-          }
-
-          sortedItems.forEach((item) =>
-            document.querySelector(".content-container").append(item)
-          );
-        }
-
-        // attach event listener to sort toggle
-        document
-          .querySelector(".sort-toggle")
-          .addEventListener("click", (e) => {
-            sortItems(e);
-          });
-
-        /* 
-        FILTER 
-        */
-
-        // create filter function
-        function filterItems(type, tag) {
-          let filteredItems;
-
-          // close all drawers after filtering
-          document
-            .querySelectorAll(".airtable-item.is-open")
-            .forEach((item) => {
-              item.classList.remove("is-open");
-            });
-
-          // filter items based on the type of the filter clicked
-          // if all is selected, reset items
-          if (tag == "all") {
-            filteredItems = allItems;
-          } else if (type == "location") {
-            filteredItems = allItems.filter((item) =>
-              item.dataset.location.includes(tag.dataset.filter)
-            );
-          }
-
-          //clear the container to make way for filtered items
-          container.innerHTML = "";
-
-          console.log('filteredItems', filteredItems);
-
-          // loop through the filtered items and append them to the container
-          filteredItems.forEach((item) => {
-            container.append(item);
-          });
-        }
-
-        // now that we have added all of the unique locations to the locations array in the forloop above,
-        // we can now loop through the locations array to create each filter
-        locations.forEach((location) => {
-          // create a filter button for each location
-          let filterBtn = document.createElement("button");
-          // add a class to the button
-          filterBtn.classList.add("filter-btn");
-          // set the data-filter equal to the value of the current array element
-          filterBtn.setAttribute("data-filter", location);
-          // render the location value of the current array element inside the html button
-          filterBtn.innerHTML = location;
-          // once the button has been created
-          // append it to the filters parent container
-          document.querySelector(".filters").append(filterBtn);
-
-          // lastly, we add an event listener
-          // which listens for a user clicking the button we've just created
-          filterBtn.addEventListener("click", (e) => {
-            // clear active states on filters
-            document
-              .querySelectorAll(".filter-btn")
-              .forEach((btn) => btn.classList.remove("is-active"));
-            // apply active state to selected filter
-            e.target.classList.add("is-active");
-            // loop through all the airtable items in the document
-            // assign type equal to 'location' and tag equal to the target
-            filterItems("location", e.target);
-          });
-        });
-
-        // add an event listener to our 'all' filter which resets the filters
-        document
-          .querySelector('.filter-btn[data-filter="all"]')
-          .addEventListener("click", (e) => {
-            document
-              .querySelectorAll(".filter-btn")
-              .forEach((btn) => btn.classList.remove("is-active"));
-            // apply active state to selected filter
-            e.target.classList.add("is-active");
-            filterItems("location", "all");
-          });
+        airtableItem.setAttribute("data-Brand", record.fields.Brand);
+        
+        // create a img tag for my album art 
+        let image = document.createElement("img");
+        image.src = record.fields.image[0].url;
+        // create a span for my artist name 
+        let Name = document.createElement("span");
+        Name.innerHTML = record.fields.Name;
+        
+        // appending to div holding each airtable record 
+        airtableItem.append(image);
+        airtableItem.append(Name);
+       // append div to body 
+        document.body.append(airtableItem);
       }
+    
+    )
+  }
+); 
+
+// set up a event listener for my empowering button 
+// listen for user clicker, once it is clicker, serach for divs with data-Brand, empowering 
+
+// get our button using css ID 
+// assign a event listener to my button to listen for click
+let empoweringFilterBtn = document.getElementById("Empowering");
+empoweringFilterBtn.addEventListener("click", function(event){
+  console.log("this is filter being pressed:", event.target.id);
+  // search my airtable-item divs, and see which data-Brand contains "empowering"
+  // put my airtable-divs in an array [airtable-div 1, airtable-div-2], find the div tht has data-Brand
+  let listofAirtableItems = document.querySelectorAll("div.airtable-item");
+  
+  // search for data-Brand, containg empowering 
+  listofAirtableItems.forEach(
+    function searchEmpoweringFilter(item){
+      // if item.dataset.Brand equal to "Empowering, then we trigger something 
+      if (item.dataset.Brand == "Empowering") {
+        // if the div has data-Brand empowering, add red background by adding css class
+        item.classList.add("empowering-filter-show");
+        console.log(item);
+      }
+      else {
+        item.classList.add("empowering-filter-hide");
+      }
+      
+      
     }
-  );
+  )
+  
+});
+
+
