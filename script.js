9b34627b8433e1bba8ec6e48cf2489ce88d42a0c
lia778 <lia778@newschool.edu>
2023-04-11 05:49:28 -0400

    YES

--- script.js
+++ script.js
@@ -34,7 +34,7 @@ base("Table 1").select({
       airtableItem.append(bag);
       airtableItem.append(Name);
       // append div to body
-      document.body.append(airtableItem);
+      container.append(airtableItem);
     });
   });
 
@@ -156,25 +156,3 @@ ALLFilterBtn.addEventListener("click", function (event) {
     item.classList.add("filter-show");
   });
 });
-
-const holographicElement = document.getElementById("holographic");
-
-function updateHolographicBackground(value) {
-  const percentage = value * 100;
-  holographicElement.style.backgroundPosition = percentage + "%";
-}
-
-function handleMouseMove(event) {
-  const x = event.clientX;
-  const width = document.documentElement.clientWidth;
-  const value = x / width;
-  updateHolographicBackground(value);
-}
-
-function handleDeviceOrientation(event) {
-  const z = Math.abs(event.alpha); // rotation degrees from 0 to 360
-  const value = z / 360;
-  updateHolographicBackground(value);
-}
-
-window.addEventListener("deviceorientation", handleDeviceOrientation, true);
